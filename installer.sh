2bab31fdb4647a812b02c19dfe5c183d7b77739b
i3-Arch <jamarlow410@st.nashcc.edu>
2014-04-17 13:16:18 -0400

    Update

--- installer.sh
+++ installer.sh
@@ -42,14 +42,14 @@ printf " What is your swap partition:\n"
 read swappart
 mkswap "$swappart" -L swapfs
 printf " Setting up install\n"
-	$(mount $rewtpart /mnt) ;
-	$(mkdir -pv /mnt/var/lib/pacman) ;
-	$(pacman -r /mnt -Sy base base-devel) ;
-	$(rsync -rav /etc/pacman.d/gnupg/ /mnt/etc/pacman.d/gnupg/) ;
-	$(mount --bind /dev/ /mnt/dev) ;
-	$(mount --bind /sys/ /mnt/sys) ;
-	$(mount --bind /proc/ /mnt/proc) ;
-	$(chroot /mnt /bin/bash) ;
+	$(mount $rewtpart /mnt)
+	$(mkdir -pv /mnt/var/lib/pacman)
+	$(pacman -r /mnt -Sy base base-devel)
+	$(rsync -rav /etc/pacman.d/gnupg/ /mnt/etc/pacman.d/gnupg/)
+	$(mount --bind /dev/ /mnt/dev)
+	$(mount --bind /sys/ /mnt/sys)
+	$(mount --bind /proc/ /mnt/proc)
+	$(chroot /mnt /bin/bash)
 printf " Setting up fstab\n"
 echo " $rewtpart    /    	ext4   defaults    0    1" >> /etc/fstab; printf "\n"
 echo " $swappart    none     swap    defaults    0    1" >> /etc/fstab; printf "\n"
