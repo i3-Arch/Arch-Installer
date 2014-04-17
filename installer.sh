d09325827fd977492f1e668974395d3f36a4b3a9
trewchainz <nate.ty@live.com>
2014-04-17 20:19:21 +0000

    modularized for execution after chroot

--- installer.sh
+++ installer.sh
@@ -12,46 +12,53 @@
 #
 #	Reminder  -  Add option for LUKS
 ############################################
-printf " WELCOME TO i3 ARCHLINUX INSTALL SCRIPT\n"
-printf " Running lsblk to list block devices\n"
-lsblk
-printf " Which Drive would you like to install to\n"
-printf " i.e - /dev/sda\n"
-printf " WARNING : /dev/sda may not be empty for you\n"
-read yourdrive
-printf " CREATE ::  boot - root -  home  - swap  partitions\n"
-printf " Would you like to use cfdisk or fdisk ?\n"
-read toolchoice
-if [ "$toolchoice" == cfdisk -o "$toolchoice" == CFDISK ]
-	then
-cfdisk $yourdrive
-	else
-fdisk $yourdrive
-fi
-printf " Enter Your Boot Partition:\n"
-read bootpart
-mkfs.ext4 "$bootpart" -L bootfs
-printf " Enter Your Root Partition:\n" 
-printf " i.e  /dev/sda1\n"
-read rewtpart
-mkfs.ext4 "$rewtpart" -L rootfs
-printf " Enter Your Home Partition:\n"
-read homepart
-mkfs.ext4 "$homepart"
-printf " What is your swap partition:\n"
-read swappart
-mkswap "$swappart" -L swapfs
-printf " Setting up install\n"
-	pacman -Syy
-	pacman -S rsync --noconfirm
-	mount $rewtpart /mnt
-	mkdir -pv /mnt/var/lib/pacman
-	pacman -r /mnt -Sy base base-devel --noconfirm
-	pacman -r /mnt -S rsync --noconfirm
-	rsync -rav /etc/pacman.d/gnupg/ /mnt/etc/pacman.d/gnupg/
-	mount --bind /dev/ /mnt/dev
-	mount --bind /sys/ /mnt/sys
-	mount --bind /proc/ /mnt/proc
+partnmnt()
+{
+	printf " WELCOME TO i3 ARCHLINUX INSTALL SCRIPT\n"
+	printf " Enter Your Root Partition\n" 
+	printf " i.e  /dev/sda1\n"
+	printf " Running lsblk to list block devices\n"
+	lsblk
+	printf " Which Drive would you like to install to\n"
+	printf " i.e - /dev/sda\n"
+	printf " WARNING : /dev/sda may not be empty for you\n"
+	read yourdrive
+	printf " CREATE ::  boot - root -  home  - swap  partitions\n"
+	printf " Would you like to use cfdisk or fdisk ?\n"
+	read toolchoice
+	if [ "$toolchoice" == cfdisk -o "$toolchoice" == CFDISK ]
+		then
+	cfdisk $yourdrive
+		else
+	fdisk $yourdrive
+	fi
+	printf " Enter Your Boot Partition:\n"
+	read bootpart
+	mkfs.ext4 "$bootpart" -L bootfs
+	printf " Enter Your Root Partition:\n" 
+	printf " i.e  /dev/sda1\n"
+	read rewtpart
+	mkfs.ext4 "$rewtpart" -L rootfs
+	printf " Enter Your Home Partition:\n"
+	read homepart
+	mkfs.ext4 "$homepart"
+	printf " What is your swap partition:\n"
+	read swappart
+	mkswap "$swappart" -L swapfs
+	printf " Setting up install\n"
+		pacman -Syy
+		pacman -S rsync --noconfirm
+		mount $rewtpart /mnt
+		mkdir -pv /mnt/var/lib/pacman
+		pacman -r /mnt -Sy base base-devel --noconfirm
+		pacman -r /mnt -S rsync --noconfirm
+		rsync -rav /etc/pacman.d/gnupg/ /mnt/etc/pacman.d/gnupg/
+		mount --bind /dev/ /mnt/dev
+		mount --bind /sys/ /mnt/sys
+		mount --bind /proc/ /mnt/proc
+	
+chnset()
+{
 	chroot /mnt /bin/bash
 	printf " Setting up fstab\n"
 	echo " $rewtpart    /    	ext4   defaults    0    1" >> /etc/fstab; printf "\n"
@@ -89,3 +96,15 @@ printf " Setting up install\n"
 	echo " initrd /boot/initramfs-linux.img " >> /mnt/boot/grub/grub.cfg; printf "\n"
 	echo "	}"
 	$(shutdown) ;
+}
+#program flow control
+main()
+{
+	partnmnt ${*}
+	chnset ${*}
+}
+
+#start program
+main ${*}
+
+#EOF
