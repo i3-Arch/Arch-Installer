25dc0d705119a3de313b8cdc33b5df2181634e6f
i3-Arch <jamarlow410@st.nashcc.edu>
2014-04-17 00:55:52 -0400

    Update | Needs WORK | BETA

--- installer.sh
+++ installer.sh
@@ -4,8 +4,12 @@
 # 	BY : i3-Arch
 #					
 #		Made to install archlinux
-#		And setup my custom i3 setup
 #		
+#		VERSION 1.0-BETA
+#	
+#	WARNING : THIS SCRIPT IS CURRENTLY BEING DEVELOPED
+#			RUN AT YOUR OWN RISK
+#
 #	Reminder  -  Add option for LUKS
 ############################################
 echo
@@ -19,7 +23,7 @@ echo " i.e - /dev/sda \n "
 echo " WARNING : /dev/sda may not be empty for you \n"
 read yourdrive
 echo
-echo " CREATE ::  root -  home  - swap  partitions \n "
+echo " CREATE ::  boot - root -  home  - swap  partitions \n "
 echo " Would you like to use cfdisk or fdisk ? \n "
 echo
 read toolchoice
@@ -29,6 +33,10 @@ if [ "$toolchoice" -eq cfdisk ]
 	else
 		$(fdisk "$youdrive")
 fi
+echo
+echo " ENTER YOUR BOOT PARTITION \n \n"
+read bootpart
+mkfs.ext4 "$bootpart" -L bootfs
 echo
 echo " Enter Your Root Partition \n " 
 echo " i.e  /dev/sda1 \n "
@@ -45,3 +53,52 @@ echo " What is your swap partition"
 echo
 read swappart
 mkswap "$swappart" -L swapfs
+echo
+echo " Setting up install "
+	$(mount $rewtpart /mnt) ;
+	$(mkdir -pv /mnt/var/lib/pacman) ;
+	$(pacman -r /mnt -Sy base base-devel) ;
+	$(rsync -rav /etc/pacman.d/gnupg/ /mnt/etc/pacman.d/gnupg/) ;
+	$(mount --bind /dev/ /mnt/dev) ;
+	$(mount --bind /sys/ /mnt/sys) ;
+	$(mount --bind /proc/ /mnt/proc) ;
+	$(chroot /mnt /bin/bash) ;
+echo
+echo " Setting up fstab"
+echo
+echo " $rewtpart    /    	ext4   defaults    0    1" >> /etc/fstab
+echo " $swappart    none     swap    defaults    0    1" >> /etc/fstab
+echo " $homepart    /home 	ext4	defaults	0	 1" >> /etc/fstab
+echo " $bootpart	/boot	ext4	defaults	0	1"	>> /etc/fstab
+echo " Choose your hostname"
+read hostresponse
+echo "$hostresponse" > /etc/hostname
+echo " Enter Your Time Zone \n \n "
+echo " CHOICES ARE  ' New York or Athens;         \n \n"
+echo " Sorry I didnt do all timezones yet \n "
+read timezoneresponse
+if [ $timezoneresponse -eq "New York" -o $timezoneresponse -eq "new york" -o $timezoneresponse -eq "NEW YORK" -o $timezoneresponse -eq " newyork " ] 
+	then
+		$(ln -s /usr/share/zoneinfo/America/New_York /etc/localtime) ;
+	elif	[ $timezoneresponse -eq "Athens" -o $timezoneresponse -eq "athens" -o $timezoneresponse -eq "ATHENS" ]
+		then
+			$( ln -s /usr/share/zoneinfo/Europe/Athens /etc/localtime) ;
+	else
+		echo " Not Understood | skipped | do it yourself |  with 'ln -s' "
+fi
+echo
+echo " YOU NOW NEED TO UNCOMMENT LOCALE "
+sleep 2
+vim /etc/local.gen
+echo
+echo " NOW GENERATING LOCALES"
+$(locale-gen) ;
+$(mkinitcpio -p linux) ;
+exit
+$(grub-install --boot-directory=/mnt/boot $bootpart) ;
+$(grub-mkconfig -o /mnt/boot/grub/grub.cfg) ;
+echo "menuentry"\ "Archlinux"\ "{" >> /mnt/boot/grub/grub.cfg
+echo "    set root=(hd0,1) " >> /mnt/boot/grub/grub.cfg
+echo " linux /boot/vmlinuz-linux root=$rewtpart " >> /mnt/boot/grub/grub.cfg
+echo " initrd /boot/initramfs-linux.img " >> /mnt/boot/grub/grub.cfg
+echo "	} "
