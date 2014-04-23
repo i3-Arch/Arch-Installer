dbdb8186dd28753f8d5a6eadeb1fd0a8adc977b5
i3-Arch <jamarlow410@st.nashcc.edu>
2014-04-23 19:14:56 -0400

    update

--- chrootnset.sh
+++ chrootnset.sh
@@ -14,7 +14,35 @@
 ############################################
 source config.sh #grab rewtpart, swappart, homepart, bootpart var values
 
-fstab() {
+decisions(){
+	if [ $thechoiceman -eq 3 ]
+		then
+			fstab3
+			elif [ $thechoiceman -eq 2 ]
+				then
+					fstab2
+		elif [ $thechoiceman -eq 1 ]
+			then
+				fstab1
+		else
+			fstab1
+	fi
+}
+
+fstab1(){
+	printf " Setting up fstab...\n"
+	echo " $rewtpart        /       ext4   defaults    0    1" >> /etc/fstab
+	echo " $bootpart        /mnt/boot       ext4    defaults        0       1"      >> /etc/fstab
+}
+
+fstab2(){
+     printf " Setting up fstab...\n"
+     echo " $rewtpart        /       ext4   defaults    0    1" >> /etc/fstab	
+     echo " $homepart        /home   ext4    defaults        0        1" >> /etc/fstab
+	 echo " $bootpart        /mnt/boot       ext4    defaults        0       1"      >> /etc/fstab	
+}
+
+fstab3() {
 	printf " Setting up fstab...\n"
 	echo " $rewtpart	/    	ext4   defaults    0    1" >> /etc/fstab
 	echo " $swappart	none     swap    defaults    0    1" >> /etc/fstab
@@ -60,7 +88,8 @@ timelocale() {
 }
 
 main(){
-	fstab
+	
+	decisions
 	hostname
 	timelocale
 	mkinitcpio -p linux
