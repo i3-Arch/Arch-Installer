b146f77a85aa4044e9aafd2fd8d89e4a8c1398d7
trewchainz <nate.ty@live.com>
2014-04-23 06:54:56 +0000

    modularized

--- chrootnset.sh
+++ chrootnset.sh
@@ -13,42 +13,59 @@
 #	Reminder  -  Add option for LUKS
 ############################################
 source config.sh #grab rewtpart, swappart, homepart, bootpart var values
-printf " Setting up fstab\n"
-echo " $rewtpart	/    	ext4   defaults    0    1" >> /etc/fstab
-echo " $swappart	none     swap    defaults    0    1" >> /etc/fstab
-echo " $homepart	/home 	ext4	defaults	0	 1" >> /etc/fstab
-echo " $bootpart	/mnt/boot	ext4	defaults	0	1"	>> /etc/fstab
-printf " Choose your hostname:\n"
-read hostresponse
-echo "$hostresponse" > /etc/hostname
-printf " \n Enter Your Time Zone:\n"
-printf " \n CHOICES ARE  ' New York or Athens ' \n"
-printf " \n Sorry I didnt do all timezones yet \n"
-printf " \n Enter (1) for New York  \n "
-printf " \n Enter (2) for Athens \n "
-read timezoneresponse
-if [ "$timezoneresponse" == NewYork -o "$timezoneresponse" == 1 ] 
-	then
-		$(ln -s /usr/share/zoneinfo/America/New_York /etc/localtime) ;
+
+fstab() {
+	printf " Setting up fstab...\n"
+	echo " $rewtpart	/    	ext4   defaults    0    1" >> /etc/fstab
+	echo " $swappart	none     swap    defaults    0    1" >> /etc/fstab
+	echo " $homepart	/home 	ext4	defaults	0	 1" >> /etc/fstab
+	echo " $bootpart	/mnt/boot	ext4	defaults	0	1"	>> /etc/fstab
+}
+
+hostname() {
+	printf " Choose your hostname:\n"
+	read hostresponse
+	echo "$hostresponse" > /etc/hostname
+}
+
+timelocale() {
+	printf " \n Enter your Time Zone:\n"
+	printf " CHOICES ARE: New York or Athens \n"
+	printf "  Sorry I didnt do all timezones yet\n"
+	printf " \n Enter (1) for New York \n "
+	printf "  Enter (2) for Athens \n "
+	read timezoneresponse
+	if [ "$timezoneresponse" == NewYork -o "$timezoneresponse" == 1 ] 
+		then
+			$(ln -s /usr/share/zoneinfo/America/New_York /etc/localtime) ;
 	elif	[ "$timezoneresponse" == Athens -o "$timezoneresponse" == 2 ]
 		then
 			$( ln -s /usr/share/zoneinfo/Europe/Athens /etc/localtime) ;
 	else
 		printf " Not Understood | skipped | do it yourself |  with 'ln -s'\n"
-fi
-printf " YOU NOW NEED TO UNCOMMENT A LOCALE\n"
-printf " Would you like to use default locale or choose your own ? \n"
-printf " Default locale is en_US.UTF-8 UTF-8 \n"
-printf " (Y) for default locale \n  (N) for choose your own \n "
-read inputscuzlocale
-if [ "$inputscuzlocale" == y -o "$inputscuzlocale" == Y ]
-	then
-		echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+	fi
+	printf " YOU NOW NEED TO UNCOMMENT A LOCALE\n"
+	printf " Would you like to use default locale or choose your own? \n"
+	printf " Default locale is en_US.UTF-8 UTF-8 \n"
+	printf " \n(Y) for default locale \n(N) for choose your own \n "
+	read inputscuzlocale
+	if [ "$inputscuzlocale" == y -o "$inputscuzlocale" == Y ]
+		then
+			echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 	else
 		nano /etc/locale.gen
-fi
-printf " NOW GENERATING LOCALES\n"
-locale-gen 
-mkinitcpio -p linux
-rm chrootnset.sh config.sh
-exit
+	fi
+	printf " NOW GENERATING LOCALES...\n"
+	locale-gen 
+}
+
+main(){
+	fstab
+	hostname
+	timelocale
+	mkinitcpio -p linux
+	rm chrootnset.sh config.sh #cleanup
+	exit 
+}
+
+main
