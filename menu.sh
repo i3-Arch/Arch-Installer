f8e516727c4f786da6be0d0e5a653be5b73f901c
i3-Arch <jamarlow410@st.nashcc.edu>
2014-04-25 00:25:41 -0400

    update

--- menu.sh
+++ menu.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-removeit=$(rm ReadMe.md 2> /dev/null)
+#
+# MENU FOR ARCH-INSTALLER
+#
+##############################
+
+scriptdir=$HOME/Arch-Installer
 red=$(tput setaf 1)
 white=$(tput setaf 7)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
+
+# SETTING THINGS UP FIRST 
+
+cd $HOME
+pacman -Syy git --noconfirm
+git clone https://github.com/i3-Arch/Arch-Installer.git
+cd $scriptdir
 
 calculate () {
 	if [ $CHOICE -eq 4 ]
@@ -25,15 +37,17 @@ clear
 	echo -e " \033[1m ${red}2)${white}\033[1m VIEW chrootnset.sh ( OTHER HALF OF SCRIPT )  \033[0m"
 	echo -e " \033[1m ${red}3)${white}\033[1m VIEW README  \033[0m"
 	echo -e " \033[1m ${red}4)${white}\033[1m INSTALL ARCHLINUX ! \033[0m"
-	echo -e " \033[1m ${red}5)${white}\033[1m Exit\033[0m"
+	echo -e " \033[1m ${red}5)${white}\033[1m CLEANUP ( RUN AFTER INSTALL | BEFORE YOU REBOOT ) \033[0m"
+	echo -e " \033[1m ${red}6)${white}\033[1m Exit\033[0m"
 	echo
 read CHOICE
 	case $CHOICE in
 		1) echo -e "\033[1m ${yellow} " && cat installer.sh|less ;;
 		2) echo -e "\033[1m ${yellow} " && cat chrootnset.sh|less ;;
-		3) echo -e "\033[1m ${white}  $(wget https://raw.githubusercontent.com/i3-Arch/Arch-Installer/master/ReadMe.md) " && cat ReadMe.md |less && sleep 2 && "$removeit" ;;
+		3) echo -e "\033[1m ${white}  " && cat ReadMe.md |less ;;
 		4) echo -e "\033[1m ${white}  LETS DO IT " && calculate ;;
-		5) echo -e "\033[1m ${yellow} HOPE YOU ENJOY \033[0m" ;;
+		5) echo -e "\033[1m ${white} " && $(rm -rf $HOME/Arch-Installer) ;;
+		6) echo -e "\033[1m ${yellow} HOPE YOU ENJOY \033[0m" ;;
 		*) echo -e "\033[1m ${yellow} invalid option ${red} try again\033[0m"
 	esac
 echo
