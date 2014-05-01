713ed7b453251b3e6eaee83074089ded35767a2b
i3-Arch <jamarlow410@st.nashcc.edu>
2014-05-01 16:50:21 -0400

    Update Post-Install.sh

--- Post-Install.sh
+++ Post-Install.sh
@@ -14,12 +14,13 @@ lemmeknow() {
 needpass() {
 	clear
 	printf " \n Would you like to set a root password ?\n \n"
-	printf " [ Yes / No ] \n"
+	printf " [Y|N] \n"
 	printf " \n \n Answer: "
 	read wutdawg
-	if [ "$wutdawg" = Y -o "$wutdawg" = y -o "$wutdawg" = YES -o "$wutdawg" = Yes -o "$wutdawg" = yes ]
+	if [ "$wutdawg" = Y -o "$wutdawg" = y ]
 		then
 			passwd
+			usersetup
 		else
 			printf " \n Ok, Moving on then.... \n "
 			usersetup
@@ -30,19 +31,19 @@ usersetup() {
 	if [ $(id -u) -eq 0 ]
 		then
 			printf " \n Would you like to setup a USER now ? \n \n"
-			printf " \n [ Yes / No ] \n "
+			printf " \n [Y|N] \n "
 			printf " \n \n Answer: "
 			read usernowbro
-			if	[ "$usernowbro" = Y -o "$usernowbro" = y -o "$usernowbro" = Yes -o "$usernowbro" = yes -o "$usernowbro" = YES ]
+			if	[ "$usernowbro" = Y -o "$usernowbro" = y ]
 				then
 				printf " \n \n Enter The Username to Create ! \n"
 				printf " \n Username: "
 				read thenameuneed
 				$(useradd -m -G disk,audio,network,video $thenameuneed)
 				printf " \n \n Set a Password for this USER ? \n \n"
-				printf " [ Yes / No ] \n \n"
+				printf " [Y|N] \n \n"
 				read cuzuwanna
-				if	[ "$cuzuwanna" = Y -o "$cuzuwanna" = y -o "$cuzuwanna" = YES -o "$cuzuwanna" = yes -o "$cuzuwanna" = Yes ]
+				if	[ "$cuzuwanna" = Y -o "$cuzuwanna" = y ]
 					then
 					passwd $thenameuneed
 					else
