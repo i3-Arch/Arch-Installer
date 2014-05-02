9b35b8f938b365ce34d586edc80624a466eb7970
trewchainz <nate.ty@live.com>
2014-05-02 00:46:48 -0700

    added mkdir mnt dev, sys, proc

--- installer.sh
+++ installer.sh
@@ -135,6 +135,9 @@ pkgmntchroot() {
 	mount $rewtpart /mnt
 	mkdir /mnt/home
 	mkdir /mnt/boot
+	mkdir /mnt/dev
+	mkdir /mnt/sys
+	mkdir /mnt/proc
 	mount $bootpart /mnt/boot
 	mount $homepart /mnt/home
 	mount --bind /dev/ /mnt/dev 	#says mount point doesn't exits
