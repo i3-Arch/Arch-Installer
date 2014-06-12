098b64cf65c93a330c6dee7b178064afe5124a47
trewchainz <nate.ty@live.com>
2014-06-11 19:32:55 -0700

    swap uuid

--- installer.sh
+++ installer.sh
@@ -117,7 +117,7 @@ FULLpart() {
 	printf "\033[0m \n ${yellow}Swap Partition: ${white}\033[0m"
 	read swappart
 	echo "swappart=$swappart" >> config.sh
-	mkswap "$swappart" -L swapfs
+	mkswap "$swappart" -U 13371337-0000-4000-0000-133700133700
 	echo "FULLpart=696" >> config.sh
 }
 
