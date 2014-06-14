28d59f8af10b2c138ea6b69f942b1ac801fb3a71
i3-Arch <i3_arch@archlinux.info>
2014-06-14 13:55:28 -0400

    Added Misc Stuff | Looks Better

--- chrootnset.sh
+++ chrootnset.sh
@@ -45,6 +45,7 @@ timelocale() {
 	printf "\033[1m \n ${yellow}Sorry I didnt do all timezones yet\n \n \033[0m"
 	printf "\033[1m \n ${white}ENTER ${green}(1)${red}for New York \n \033[0m"
 	printf "\033[1m    ${white}ENTER ${green}(2)${red}for Athens \n \033[0m"
+	printf "\033[1m \n ${white} Choice: \033[0m"
 	read timezoneresponse
 	if [ "$timezoneresponse" == NewYork -o "$timezoneresponse" == 1 ]
 		then
@@ -59,6 +60,7 @@ timelocale() {
 	printf "\033[1m ${green}Would you like to use default locale or choose your own? \n \033[0m"
 	printf "\033[1m ${white}Default locale is ${red}en_US.UTF-8 UTF-8 \n \033[0m"
 	printf "\033[1m \n${yellow}(Y)${green} for default locale \n${yellow}(N) ${green}for choose your own \n \033[0m"
+	printf "\033[1m \n ${white}Choice: \033[0m"
 	read inputscuzlocale
 	if [ "$inputscuzlocale" == y -o "$inputscuzlocale" == Y ]
 		then
