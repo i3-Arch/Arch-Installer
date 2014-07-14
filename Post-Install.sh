0c1a5ecbb1b0bc1751c0e5f91affbe5d736e49b5
i3-Arch <i3_arch@archlinux.info>
2014-07-14 01:32:12 -0400

    update | randomness

--- Post-Install.sh
+++ Post-Install.sh
@@ -12,13 +12,24 @@ white=$(tput setaf 7)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 
+updateUpgrade() {
+		if [ $(id -u) -eq 0 ]
+			then
+				pacman -Syyu --noconfirm
+			else
+				printf "\033[1m ${green} \n\nEnter Pass For Root ${yellow}( For Update/Upgrade ) \033[0m"
+				su root
+				pacman -Syyu --noconfirm
+		fi
+}
+
 mirrorSelect() {
-	printf "\033[1m ${green} \n\n Select Your Mirrors \n\n \033[0m"
-	sleep 2
-	if [ -f /etc/pacman.d/mirrorlist.pacnew ]
-		then
-			cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist
-	fi
+		printf "\033[1m ${green} \n\n Select Your Mirrors \n\n \033[0m"
+		sleep 2
+			if [ -f /etc/pacman.d/mirrorlist.pacnew ]
+				then
+					cp /etc/pacman.d/mirrorlist.pacnew /etc/pacman.d/mirrorlist
+			fi
 	nano /etc/pacman.d/mirrorlist
 }
 
@@ -60,10 +71,18 @@ usersetup() {  ## CALLED BY needpass
 				printf "\033[1m \n\n ${green} Enter The Username to Create ! \n \033[0m"
 				printf "\033[1m \n Username:${white} \033[0m"
 				read thenameuneed
-				$(useradd -m -G disk,audio,network,video $thenameuneed)
+				$(useradd -m -G adm,disk,audio,network,video $thenameuneed)
 				printf "\033[1m \n\n ${yellow} Set a Password for this USER now \n\n \033[0m"
 				printf "\033[1m \n ${red} If you dont you will not be able to use it .\n \033[0m"
 				passwd $thenameuneed
+				printf "\033[1m \n\n ${yellow}Would you like to add user to sudoers? ( user ALL=(ALL) ALL ) \033[0m"
+				printf "\033[1m \n\n ${white}[${green}Y${white}|${red}N${white}] \033[0m"
+				printf "\033[1m\n\n ${red}Answer: ${white}\033[0m"
+				read anot
+				if [ "$anot" == Y -o "$anot" == y -o "$anot" == yes -o "$anot" == YES ]
+					then
+						echo -n "$thenameuneed" "ALL=(ALL)" "ALL" >> /etc/sudoers
+				fi
 				else
 				printf "\033[1m \n ${green} Moving on \n \033[0m"
 			fi
@@ -75,7 +94,7 @@ usersetup() {  ## CALLED BY needpass
 uwantme() {
 	clear
 	printf "\033[1m \n ${green} Do you want to install a WM/DE now ? \n \033[0m"
-	printf "\033[1m \n\n ${yellow} [Y/N] \n\n \033[0m"
+	printf "\033[1m \n\n ${yellow} [Y|N] \n\n \033[0m"
 	printf "\033[1m \n\n ${red} Choice:${white} \033[0m"
 	read wutdebro
 	if [ "$wutdebro" == Y -o "$wutdebro" == y -o "$wutdebro" == YES -o "$wutdebro" == yes ]
@@ -117,6 +136,7 @@ thankyoubro() {
 }
 
 main() {
+	updateUpgrade
 	thankyoubro
 	lemmeknow
 	mirrorSelect
