0450d98c3bb57eb8b2204f62cabde1c3036d0298
i3-Arch <i3_arch@archlinux.info>
2014-07-16 16:43:27 -0400

    color missing

--- de-wm.sh
+++ de-wm.sh
@@ -52,7 +52,7 @@ makeitbro() {
 		printf "\033[1m \n\n ${green}Option 2: ${yellow}Install Our CUSTOM i3 Setup\n \033[0m"
 		printf "\033[1m \n\n ${green}Option 3: ${yellow}Install Default Cinnamon Setup \n\n \033[0m" 
 		printf "\033[1m \n\n ${green}Option 4: ${yellow}Install Default Dwm Setup \n\n \033[0m"
-		printf "\033[1m\n ${green}Choose ${red}1${white},${red}2${white},${red}3${white} or 4${white}: \033[0m"
+		printf "\033[1m\n ${green}Choose ${red}1${white},${red}2${white},${red}3${white} or ${red}4${white}: \033[0m"
 		read DemChoice
 		if [ "$DemChoice" == 1 ]
 			then
