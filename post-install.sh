ad96abbe8ae49c45cd6796e1889a542112a3b39c
i3-Arch <jamarlow410@st.nashcc.edu>
2014-07-23 16:00:30 -0400

    fixed

--- post-install.sh
+++ post-install.sh
@@ -367,7 +367,7 @@ bobthebuilder() {
 	if [ "$thatquestion" == Y -o "$thatquestion" == y ]
 		then
 		printf "\033[1m\n\n ${green}Setting up pacaur for future use \n\n\033[0m"
-		pacman -Syy expac --noconfirm
+		pacman -Syy expac yajl --noconfirm
 		su "$namebro" -c "mkdir /home/"$namebro"/build-dir"
 		su "$namebro" -c "cd /home/"$namebro"/build-dir && wget https://aur.archlinux.org/packages/co/cower/cower.tar.gz && tar xzvf cower.tar.gz"
 		su "$namebro" -c "cd /home/"$namebro"/build-dir/cower && makepkg -s"
