2a23fc55e4917fd8b1118ff29688da86838a0b57
i3-Arch <i3_arch@archlinux.info>
2014-07-23 03:03:24 -0400

    update | OCD edit

--- menu.sh
+++ menu.sh
@@ -13,6 +13,7 @@ green="$(tput setaf 2)"
 yellow="$(tput setaf 3)"
 
 # SETTING THINGS UP FIRST 
+clear
 printf " \033[1m ${red} \n Setting Things Up First \n \033[0m "
 cd "$HOME"
 pacman -Syy git --noconfirm
