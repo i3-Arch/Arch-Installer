23c6d8167631d7a6ece8b23e085b2b1ab0c6070d
i3-Arch <jeremymarlow@ymail.com>
2021-10-11 20:49:21 -0400

    update...

--- wired-setup.sh
+++ wired-setup.sh
@@ -3,6 +3,15 @@
 ## i3-Arch  ##   So simple, a caveman could write it....
 ##############
 
+ip a
+printf "\033[1m ${green}  [ ie: what starts with enp (( enp0s3 )) ]\n Enter your iface name:  \033[0m"
+read ifacename
+
+echo "[Match]" > /etc/systemd/network/20-wired.network
+echo "Name=$ifacename" >> /etc/systemd/network/20-wired.network
+echo "[Network]" >> /etc/systemd/network/20-wired.network
+echo "DHCP=yes" >> /etc/systemd/network/20-wired.network
+
 systemctl start systemd-networkd.service
 systemctl enable systemd-networkd.service
 
